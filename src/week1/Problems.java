98cc61d9d97625db7dd424d941a166a079f1373b
hbahramian <habahram@iu.edu>
2024-01-07 11:52:51 -0500

    .

--- src/week1/Problems.java
+++ src/week1/Problems.java
@@ -4,6 +4,15 @@ public class Problems {
 
     public static int[] problem1(int[] numbers, int target) {
         int[] indices = {-1, -1};
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                if(numbers[i] + numbers[j] == target) {
+                    indices[0] = i;
+                    indices[1] = j;
+                    break;
+                }
+            }
+        }
         return indices;
     }
 }
