0ec577fa517658bfe523b5a2345831ef504b8bfa
hbahramian <habahram@iu.edu>
2024-01-07 13:41:20 -0500

    .

--- src/week1/Problems.java
+++ src/week1/Problems.java
@@ -15,4 +15,42 @@ public class Problems {
         }
         return indices;
     }
+
+    public static boolean isPalindrome(int x) {
+        boolean result = true;
+        if(x < 0) {
+            return false;
+        }
+        String s = String.valueOf(x);
+        int n = s.length();
+        for(int i = 0; i < n / 2; i++) {
+            System.out.println(s.charAt(i));
+            if(s.charAt(i) != s.charAt(n - 1 - i)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static String longestCommonPrefix(String[] strings) {
+        String answer = "";
+        String s = strings[0];
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            boolean isCommon = true;
+            for(int j = 1; j < strings.length; j++) {
+
+                if((i > strings[j].length() - 1) ||  (strings[j].charAt(i) != c)) {
+                    isCommon = false;
+                    break;
+                }
+            }
+            if(isCommon) {
+                answer += c;
+            } else break;
+        }
+
+        return answer;
+    }
 }
